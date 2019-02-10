9e77a60a8aa7ce468e69e7e678beac1ceb72e6eb
spthermo <spthermo@e-ce.uth.gr>
2019-02-11 00:40:12 +0200

    preliminary work on the model

--- train.py
+++ train.py
@@ -14,9 +14,191 @@ import torch.utils.data
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-
+from torch.nn.init import xavier_uniform_, zeros_
 from logger import Logger
 
 # hard-wire the gpu id
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--cuda', default=1, action='store_true', help='enables cuda')
+parser.add_argument('--ndf', type=int, default=64, help='num of channels in conv layers')
+
+opt = parser.parse_args()
+print(opt)
+
+if opt.manualSeed is None:
+    opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+
+cudnn.benchmark = True
+
+device = torch.device("cuda:0" if opt.cuda else "cpu")
+
+ndf = opt.ndf
+
+def conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
+        nn.ReLU(True)
+    )
+
+def tconv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1),
+        nn.ReLU(True)
+    )
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+        self.conv1 = conv(3, ndf)
+        self.conv2 = conv(ndf, 2 * ndf)
+        self.conv3 = conv(2 * ndf, 4 * ndf)
+        self.conv4 = conv(4 * ndf, 8 * ndf)
+        self.conv5 = conv(8 * ndf, 8 * ndf)
+        self.conv6 = conv(8 * ndf, 8 * ndf)
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    zeros_(m.bias)
+
+    def forward(self, x):
+        out_conv1 = self.conv1(x);
+        out_conv2 = self.conv2(out_conv1);
+        out_conv3 = self.conv3(out_conv2);
+        out_conv4 = self.conv4(out_conv3);
+        out_conv5 = self.conv5(out_conv4);
+        out_conv6 = self.conv6(out_conv5);
+
+        return out_conv6
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.tconv1 = tconv(8 * ndf, 8 * ndf)
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    zeros_(m.bias)
+
+    def forward(self, x):
+        out_tconv1 = self.tconv1(x);
+
+        return out_tconv1
+
+
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_channels, hidden_channels, kernel_size, bias=True):
+        super(ConvLSTMCell, self).__init__()
+
+        assert hidden_channels % 2 == 0
+
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.bias = bias
+        self.kernel_size = kernel_size
+        self.num_features = 4
+
+        self.padding = int((kernel_size - 1) / 2)
+
+        self.Wxi = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Whi = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxf = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Whf = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxc = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.Whc = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+        self.Wxo = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding,  bias=True)
+        self.Who = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        self.Wci = None
+        self.Wcf = None
+        self.Wco = None
+
+    def forward(self, x, h, c):
+        ci = torch.sigmoid(self.Wxi(x) + self.Whi(h) + c * self.Wci)
+        cf = torch.sigmoid(self.Wxf(x) + self.Whf(h) + c * self.Wcf)
+        cc = cf * c + ci * torch.tanh(self.Wxc(x) + self.Whc(h))
+        co = torch.sigmoid(self.Wxo(x) + self.Who(h) + cc * self.Wco)
+        ch = co * torch.tanh(cc)
+        return ch, cc
+
+    def init_hidden(self, batch_size, hidden, shape):
+        self.Wci = Variable(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+        self.Wcf = Variable(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+        self.Wco = Variable(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+        return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda(),
+                Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda())
+
+
+class ConvLSTM(nn.Module):
+    # input_channels corresponds to the first input feature map
+    # hidden state is a list of succeeding lstm layers.
+    def __init__(self, input_channels, hidden_channels, kernel_size, step=1, effective_step=[1], bias=True):
+        super(ConvLSTM, self).__init__()
+        self.input_channels = [input_channels] + hidden_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+        self.num_layers = len(hidden_channels)
+        self.step = step
+        self.bias = bias
+        self.effective_step = effective_step
+        self._all_layers = []
+        for i in range(self.num_layers):
+            name = 'cell{}'.format(i)
+            cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size, self.bias)
+            setattr(self, name, cell)
+            self._all_layers.append(cell)
+
+    def forward(self, input):
+        internal_state = []
+        outputs = []
+        for step in range(self.step):
+            x = input
+            for i in range(self.num_layers):
+                # all cells are initialized in the first step
+                name = 'cell{}'.format(i)
+                if step == 0:
+                    bsize, _, height, width = x.size()
+                    (h, c) = getattr(self, name).init_hidden(batch_size=bsize, hidden=self.hidden_channels[i], shape=(height, width))
+                    internal_state.append((h, c))
+
+                # do forward
+                (h, c) = internal_state[i]
+                x, new_c = getattr(self, name)(x, h, c)
+                internal_state[i] = (x, new_c)
+            # only record effective steps
+            if step in self.effective_step:
+                outputs.append(x)
+
+        return outputs, (x, new_c)
+
+if __name__ == '__main__':
+
+    encoder = Encoder().to(device)
+    encoder.init_weights()
+    decoder = Decoder().to(device)
+    decoder.init_weights()
+
+    input = Variable(torch.randn(32, 3, 512, 512)).cuda()
+    out_encoder = encoder.forward(input)
+    
+    convlstm = ConvLSTM(input_channels=512, hidden_channels=[512, 512], kernel_size=3, step=5, effective_step=[4]).cuda()
+    out_lstm, _ = convlstm(out_encoder)
+    
+    out_decoder = decoder.forward(out_lstm[0])
+    print(out_decoder.size())
+
